cce1a41e0b14d08add66b9415e28ed5b99844f50
Michael A. Papachatzakis <michael_met@hotmail.com>
2025-07-10 18:31:20 +0300

    Added entry and exit macros along with init and exit attributes

--- acc_mod.c
+++ acc_mod.c
@@ -1,27 +1,21 @@
 #include <linux/module.h>
-//#include <linux/printk.h> 
+#include <linux/init.h> 
 
- 
 
-int init_module(void) 
-
+static int __init init_acc_mod(void)
 { 
-
     pr_info("Hello world!\n"); 
-    return 0; 
-
+    return 0;
 } 
 
- 
-
-void cleanup_module(void) 
-
+static void __exit exit_acc_mod(void) 
 { 
-
     pr_info("Goodbye world!\n"); 
-
 } 
 
- 
+module_init(init_acc_mod);
+module_exit(exit_acc_mod);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michael A. Papachatzakis");
+MODULE_DESCRIPTION("Kernel module for the FPGA accelerator");
