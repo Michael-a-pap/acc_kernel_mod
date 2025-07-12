2996d5153c06fb7c3bba6b8e122486bf6f413681
Michael A. Papachatzakis <michael_met@hotmail.com>
2025-07-12 04:48:25 +0300

    Major number implementation

--- acc_mod.c
+++ acc_mod.c
@@ -1,16 +1,27 @@
 #include <linux/module.h>
 #include <linux/init.h> 
+#include <linux/fs.h>
+
+static int major;
+
+static struct file_operations fops = {};
 
 
 static int __init init_acc_mod(void)
-{ 
-    pr_info("Hello world!\n"); 
-    return 0;
+{
+	major = register_chrdev(0, "fpga_accelerator", &fops); //0 is for dynamic allocation
+	if (major < 0) {
+		pr_info("Error registering accelerator device");
+		return major;
+	}
+	pr_info("FPGA accelerator - Major Device Number: %d\n", major); 
+	return 0;
 } 
 
 static void __exit exit_acc_mod(void) 
 { 
-    pr_info("Goodbye world!\n"); 
+	unregister_chrdev(major, "fpga_accelerator");
+	pr_info("FPGA accelerator - Unregistering"); 
 } 
 
 module_init(init_acc_mod);
